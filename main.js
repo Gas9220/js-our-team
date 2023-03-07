f5a00d5b592d63bad5cb4329e41795615a657e2f
Gaspare <Gas9220@users.noreply.github.com>
2023-03-07 14:17:16 +0100

    Milestone 0: Creato array di oggetti

--- main.js
+++ main.js
@@ -12,10 +12,16 @@ function createTeamMember(name, role, image) {
     return teamMember;
 }
 
+let teamMembers = [];
+
 let wayneBarret = createTeamMember("Wayne Barret", "Founder & CEO", "wayne-barret-founder-ceo.jpg");
 let angelaCaroll = createTeamMember("Angela Caroll", "Chief Editor", "angela-caroll-chief-editor.jpg");
 let walterGordon = createTeamMember("Walter Gordon", "Office Manager", "walter-gordon-office-manager.jpg");
 let angelaLopez = createTeamMember("Angela Lopez", "Social Media Manager", "angela-lopez-social-media-manager.jpg");
 let scottEstrada = createTeamMember("Scott Estrada", "Developer", "scott-estrada-developer.jpg");
 let barbaraRamos = createTeamMember("Barbara Ramos", "Graphic Designer", "barbara-ramos-graphic-designer.jpg");
+
+teamMembers.push(wayneBarret, angelaCaroll, walterGordon, angelaLopez, scottEstrada, barbaraRamos);
+
+console.log("teamMembers", teamMembers);
 
