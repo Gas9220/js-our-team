b155c1362f9377f000878edc2ae67423e919250a
Gaspare <Gas9220@users.noreply.github.com>
2023-03-07 14:18:24 +0100

    Aggiunti commenti

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Funzione che facilita la creazione di un singolo membro
 function createTeamMember(name, role, image) {
     let teamMember = {
         name: name,
@@ -12,8 +13,10 @@ function createTeamMember(name, role, image) {
     return teamMember;
 }
 
+// Array che conterrà i membri
 let teamMembers = [];
 
+// Creo i membri
 let wayneBarret = createTeamMember("Wayne Barret", "Founder & CEO", "wayne-barret-founder-ceo.jpg");
 let angelaCaroll = createTeamMember("Angela Caroll", "Chief Editor", "angela-caroll-chief-editor.jpg");
 let walterGordon = createTeamMember("Walter Gordon", "Office Manager", "walter-gordon-office-manager.jpg");
@@ -21,6 +24,7 @@ let angelaLopez = createTeamMember("Angela Lopez", "Social Media Manager", "ange
 let scottEstrada = createTeamMember("Scott Estrada", "Developer", "scott-estrada-developer.jpg");
 let barbaraRamos = createTeamMember("Barbara Ramos", "Graphic Designer", "barbara-ramos-graphic-designer.jpg");
 
+// Aggiungo i membri all'array
 teamMembers.push(wayneBarret, angelaCaroll, walterGordon, angelaLopez, scottEstrada, barbaraRamos);
 
 console.log("teamMembers", teamMembers);
