02ada3dc9378896c51b8774da08d0ddb97a62e54
Gaspare <Gas9220@users.noreply.github.com>
2023-03-07 14:16:02 +0100

    Creati i membri del team

--- main.js
+++ main.js
@@ -11,3 +11,11 @@ function createTeamMember(name, role, image) {
 
     return teamMember;
 }
+
+let wayneBarret = createTeamMember("Wayne Barret", "Founder & CEO", "wayne-barret-founder-ceo.jpg");
+let angelaCaroll = createTeamMember("Angela Caroll", "Chief Editor", "angela-caroll-chief-editor.jpg");
+let walterGordon = createTeamMember("Walter Gordon", "Office Manager", "walter-gordon-office-manager.jpg");
+let angelaLopez = createTeamMember("Angela Lopez", "Social Media Manager", "angela-lopez-social-media-manager.jpg");
+let scottEstrada = createTeamMember("Scott Estrada", "Developer", "scott-estrada-developer.jpg");
+let barbaraRamos = createTeamMember("Barbara Ramos", "Graphic Designer", "barbara-ramos-graphic-designer.jpg");
+
